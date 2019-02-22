bc1c3ea0d53c7d7263766d16a9ef49d3d6fc20a1
Greg Cook <greg@gregcook.com>
2019-02-22 17:42:37 +1100

    fix secret variable name

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ set -e
 case "${PLUGIN_STATUS}" in
 	started)
 		curl -sS https://api.rollbar.com/api/1/deploy/ \
-			-F access_token="${PLUGIN_ROLLBAR_ACCESS_TOKEN}" \
+			-F access_token="${ROLLBAR_ACCESS_TOKEN}" \
 			-F environment="${PLUGIN_ROLLBAR_ENV}" \
 			-F revision="${DRONE_COMMIT_SHA}" \
 			-F local_username="${DRONE_COMMIT_AUTHOR}" \
@@ -16,12 +16,12 @@ case "${PLUGIN_STATUS}" in
 	updated)
 		case "${DRONE_JOB_STATUS}" in
 			success)
-				curl -X PATCH -sS https://api.rollbar.com/api/1/deploy/"$(cat rollbar_deploy_id.txt)"/?access_token="${PLUGIN_ROLLBAR_ACCESS_TOKEN}" \
+				curl -X PATCH -sS https://api.rollbar.com/api/1/deploy/"$(cat rollbar_deploy_id.txt)"/?access_token="${ROLLBAR_ACCESS_TOKEN}" \
 					-F status="succeeded"
 				echo "Rollbar notified of successful deployment, ID: $(cat rollbar_deploy_id.txt)"
 		    ;;
 			failure)
-				curl -X PATCH -sS https://api.rollbar.com/api/1/deploy/"$(cat rollbar_deploy_id.txt)"/?access_token="${PLUGIN_ROLLBAR_ACCESS_TOKEN}" \
+				curl -X PATCH -sS https://api.rollbar.com/api/1/deploy/"$(cat rollbar_deploy_id.txt)"/?access_token="${ROLLBAR_ACCESS_TOKEN}" \
 					-F status="failed"
 				;;
 		esac
